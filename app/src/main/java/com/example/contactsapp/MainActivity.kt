e3e4d9b211a814ae207d41c62c9e15816bda399d
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-06-05 11:12:31 +0300

    Battery connection receiver

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.contactsapp
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.BatteryManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
+import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
@@ -18,25 +23,16 @@ import contacts.core.util.organizationList
 import contacts.core.util.phoneList
 
 
-val CONTACT_PROJECTION: Array<out String> = arrayOf(
-    ContactsContract.Data._ID,
-    ContactsContract.Contacts.LOOKUP_KEY,
-    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
-    ContactsContract.CommonDataKinds.Phone.NUMBER,
-    ContactsContract.CommonDataKinds.Organization.COMPANY,
-    ContactsContract.CommonDataKinds.Email.ADDRESS,
-    ContactsContract.Data.MIMETYPE
-)
-
 const val TAG = "MyApp"
 
 class MainActivity : AppCompatActivity() {
-
+    var batteryStatus: Intent? = null
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         if (!readContactsPermissionGranted()) {
             requestReadContactsPermission()
@@ -48,10 +44,10 @@ class MainActivity : AppCompatActivity() {
                 ContactsData.ContactData(
                     contact.id,
                     contact.displayNamePrimary,
-                    contact.phoneList().map{ it.number ?: "no phone number" },
-                    contact.organizationList().map{ it.company ?: "no organization found" },
-                    contact.emailList().map{ it.address ?: "no email address found" }
-                    )
+                    contact.phoneList().map { it.number ?: "no phone number" },
+                    contact.organizationList().map { it.company ?: "no organization found" },
+                    contact.emailList().map { it.address ?: "no email address found" }
+                )
             )
         }
 
@@ -78,4 +74,37 @@ class MainActivity : AppCompatActivity() {
         Manifest.permission.READ_CONTACTS
     ) == PackageManager.PERMISSION_GRANTED
 
+    override fun onStart() {
+        super.onStart()
+        batteryStatus = IntentFilter(Intent.ACTION_POWER_CONNECTED).let { ifilter ->
+            this.registerReceiver(BatteryStatusReceiver(), ifilter)
+        }
+
+        val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+        val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING
+                || status == BatteryManager.BATTERY_STATUS_FULL
+
+        findViewById<TextView>(R.id.tvBattery).text = if (isCharging) "Charging connected" else "Charging disconnected"
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        this.unregisterReceiver(BatteryStatusReceiver())
+    }
+
+    inner class BatteryStatusReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val tvBatteryStatus = findViewById<TextView>(R.id.tvBattery)
+            if (intent != null) {
+                if(intent.action == Intent.ACTION_POWER_CONNECTED){
+                    Log.d(TAG, "charger connected")
+                    tvBatteryStatus.text = "Charging connected"
+                }else if (intent.action == Intent.ACTION_POWER_DISCONNECTED){
+                    Log.d(TAG, "charger connected")
+                    tvBatteryStatus.text = "Charging disconnected"
+                }
+            }
+        }
+    }
 }
